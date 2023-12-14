9ed95985e5f96c234a11071f7182576d8d086fc7
johnmoreira <johnlennon_moreira@hotmail.com>
2023-12-14 16:37:03 -0300

    ye

--- src/app/dashboard/components/card-loja/card-loja.component.ts
+++ src/app/dashboard/components/card-loja/card-loja.component.ts
@@ -11,4 +11,16 @@ export class CardLojaComponent {
   @Input() txtPrincipal: string = '1.00';
   @Input() txtSecundario: string = '0,0%';
   @Input() ordem: string = '';
+
+
+  @Input() notaMes: string = '0.00';
+  @Input() percentualNota: string = '0.0%';
+
+  @Input() lojasPerfeitas: string = '0';
+  @Input() lojasVisitadas: string = '0';
+
+  @Input() percentualLojasPerfeitas: string = '0.0%';
+  @Input() totalLojas: string = '0';
+  @Input() percentualVisitadas: string = '0.0%';
+
 }
